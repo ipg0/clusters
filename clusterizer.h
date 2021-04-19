ab50a7aab584fba4f3d13741903b1da19fb0bbb3
notblank00 <pyromancy00@gmail.com>
2021-04-19 21:44:40 +0300

    cluster-fucking around with headers lol

--- clusterizer.h
+++ clusterizer.h
@@ -5,18 +5,19 @@
 #include "field.h"
 
 class ISearchAlgorithm {
-private:
-    Field field;
 public:
-    virtual Cloud *search() = 0;
+    virtual Cloud *search(Field *_field, size_t _number) = 0;
 };
 
-class Clusterizer {
+class Clusterizer { // I really want to rename this into "clusterfuck" at this point
 private:
-    Field field;
-    ISearchAlgorithm currentSearchAlgorithm;
+    Field *field;
+    ISearchAlgorithm *currentSearchAlgorithm;
 public:
-    Clusterizer(Field _field, ISearchAlgorithm _searchAlgorithm)
+    Clusterizer(Field *_field) { field = _field; currentSearchAlgorithm = nullptr; }
+    Clusterizer(Field *_field, ISearchAlgorithm *_searchAlgorithm);
+    Cloud *search(size_t _number) { return currentSearchAlgorithm.search(field, _number); }
+    void updateSearchAlgorithm(ISearchAlgorithm *_searchAlgorithm) { currentSearchAlgorithm = _searchAlgorithm; }
 };
 
 #endif
