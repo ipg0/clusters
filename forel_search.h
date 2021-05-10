a3b50ace415fe155a8d3a75a425f9f40550fd7d6
notblank00 <pyromancy00@gmail.com>
2021-05-10 09:29:11 +0300

    apparently, you can't define private methods in a .cpp

--- forel_search.h
+++ forel_search.h
@@ -9,12 +9,12 @@ private:
     size_t markedPoints;
     Point currentCenter;
     Field *field;
+public:
     size_t randomPointIndex() const;
     Point **getPointsWithinRadius(Point &center, size_t &quantity) const;
     Point centerOfMass(Point **points, size_t quantity) const;
     bool validate(Point &point) const { return point.clusterDefined(); }
     bool allPointsMarked() const { return markedPoints == field->getQuantity(); }
-public:
     FOREL(Field *_field) { field = _field; markedPoints = 0; }
     void reset() { markedPoints = 0; field->reset(); };
     virtual size_t search(double floatingParameter) override;
